bd49911eca17e1dc691a85c9781c58431a25ca32
Le-foucheur <38878602+Le-foucheur@users.noreply.github.com>
2024-09-14 19:27:37 +0200

    add

---
+++ waybar/modules/spotify.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+class=$(playerctl metadata --player=Deezer --format '{{lc(status)}}')
+icon=""
+
+if [[ $class == "playing" ]]; then
+  info=$(playerctl metadata --player=Deezer --format '{{title}} - {{artist}}')
+  info=$(echo ${info//\"/\\\"}) # Escape quote with \"
+  if [[ ${#info} -ge 33 ]]; then
+    info=$(playerctl metadata --player=Deezer --format '{{title}}')
+    info=$(echo ${info//\"/\\\"}) # Escape quote with \"
+  fi
+  if [[ ${#info} -ge 33 ]]; then
+    info=$(echo $info | cut -c1-30)"..."
+  fi
+  tooltip=$(playerctl metadata --format "{{duration(position)}}/{{duration(mpris:length)}}")
+  text=$info" "$icon
+  tooltip="🜁 "$tooltip" 🜃"
+elif [[ $class == "paused" ]]; then
+  text="󰏤 "$icon
+elif [[ $class == "stopped" ]]; then
+  text=""
+fi
+
+echo -e "{\"text\":\""$text"\", \"tooltip\":\""$tooltip"\"}, \"class\":\""$class"\"}"
